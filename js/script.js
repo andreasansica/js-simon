26fa9c29123db2bfa6aba562d36e5bab5bce3c0b
Andrea Sansica <sansica.andrea99@gmail.com>
2020-11-05 17:28:24 +0100

    problema contatore

---
+++ js/script.js
@@ -0,0 +1,73 @@
+
+
+// Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
+
+
+// variabili
+var numeriRandom;
+var numeri = [];
+var numeriUtente = [];
+var inputUtente;
+var numeroOk;
+var count = 0;
+var tentativi = 5;
+
+// genero lista numeri random
+
+while (numeri.length < 5) {
+  numeriRandom = Math.floor(Math.random() * 100) + 1;
+
+  // verifico che il numero non ci sia già
+  if(!numeri.includes(numeriRandom)) {
+  numeri.push(numeriRandom);
+}
+}
+ console.log("I numeri random sono: " + numeri);
+
+
+// dopo 30 secondi faccio partire i prompt
+
+setTimeout(myFunction, 3000);
+  function myFunction(){
+
+    // input utente
+    for (var i = 0; i < 5; i++) {
+      inputUtente = parseInt(prompt("Inserisci uno dei 5 numeri mostrati prima"));
+
+      // verifico che l'utente non ripeta due volte lo stessto numero
+      if(!numeriUtente.includes(inputUtente)) {
+      numeriUtente.push(inputUtente);
+    } else {
+       alert("Non puoi inserire 2 volte lo stesso numero");
+       i--;
+    }
+    }
+     console.log("I numeri inseriti dall'utente sono: " + numeriUtente);
+  }
+
+
+
+
+
+    while(numeroOk === false && numeriUtente.length <= tentativi){
+  // se il numero dell'utente è sbagliato
+  if (numeroOk = false){
+    numeri.includes(inputUtente)
+  }
+  // altrimenti se è giusto
+  else if (numeroOk = true) {
+    numeriUtente.push(inputUtente);
+    count ++;
+
+  }
+}
+
+setTimeout(risultato, 3000);
+function risultato(){
+console.log("Hai indovinato " + count + " numeri!");
+
+}
